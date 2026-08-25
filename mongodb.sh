2c1ed95f3b7c55fb8ba199fdd547fedd5dc544e0
bhargavidevops1 <bhargavibysani2001@gmail.com>
2026-08-25 22:30:58 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -5,6 +5,7 @@ sudo mkdir -p $log_folder
 sudo chown -R ec2-user:ec2-user $log_folder
 sudo chmod -R 755 $log_folder 
 logfile="$log_folder/$0.log"
+Timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
 userid=$(id -u)
 R="\e[31m"
@@ -13,8 +14,17 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ $userid -ne 0 ]; then
-echo -e " $R please login with root $N " | tee -a $logfile
+echo -e "$Timestamp [error] $R please login with root $N " | tee -a $logfile
 exit 1
 fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate(){
+    if [ $1 -ne 0 ]; then
+    echo -e "$Timestamp [ERROR] $2.... $R Failure $N" | tee -a $logfile
+    exit 1
+    else
+    echo -e "$Timestamp [INFO] $2.... $G SUCCESS $N" | tee -a $logfile
+    fi
+}
+validate $? "adding mongo db"
